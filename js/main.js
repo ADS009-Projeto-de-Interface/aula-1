b6e017a7935e282edec6ba87585d7919f9de518a
sergiobslva-iesb <sergio.b.silva@iesb.edu.br>
2024-03-05 10:40:06 -0300

    Adicionando estrutura do repositório

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var N1 = prompt("Informe um número");
+
+if(N1 == 0) {
+     console.log("O número " + N1 + "é ZERO!");
+}
+
+else(N1 > 0) {
+     console.log("O número " + N1 + "é POSITIVO!");
+}
+
+else {
+     console.log("O número " + N1 + "é NEGATIVO!");
+}
